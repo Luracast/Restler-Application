db6e6d1b88905a2b868d7b51cb103e084d8c018b
Luracast <arul@luracast.com>
2015-09-17 23:29:08 +0800

    Improves the php development server to better find static files by removing the query string from url

--- server.php
+++ server.php
@@ -1,7 +1,9 @@
 <?php
 define('WEB_FOLDER', __DIR__ . '/public/');
 
-if (file_exists(WEB_FOLDER . $_SERVER['REQUEST_URI'])) {
+$request = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+
+if (file_exists(WEB_FOLDER . $request)) {
     return false; // serve the requested resource as-is.
 } else {
     include WEB_FOLDER . 'index.php';
